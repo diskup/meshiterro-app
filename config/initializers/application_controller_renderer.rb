50f387575d9f6a3c36b9eb6f210cd6d46b4f888e
diskup <taigaa.1998@gmail.com>
2021-10-17 08:25:36 +0000

    -4まで

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'af21fb95e570a5285f938b7436caf4ce09396d9989b4bf26ac2ee8afb72b018d237c028aa5f3a911a5312e1eb1c389d6c48d1e148878c4b52b4d7f3ffdb73df2'
